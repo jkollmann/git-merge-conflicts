405186ebcf90c2b9c4c3b263fea546d05741a440
SuperCoder <supercoder@SuperCoders-MacBook-Air.local>
2022-12-07 09:27:09 +0100

    optional chaining

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,11 +3,13 @@ import NewsArticle from "../../components/newsarticle/NewsArticle";
 import "./Home.css";
 
 function Home() {
-    const [articles, setArticles] = useState([]);
+    // 1. render: articles = undefined
+    const [articles, setArticles] = useState();
 
     useEffect(() => {
         console.log("Getting new articles from the server");
 
+        // Fetch wird asynchron ausgeführt, d.h. articles wird nicht sofort gesetzt
         fetch(`https://newsapi.org/v2/top-headlines?country=de&apiKey=${process.env.REACT_APP_API_KEY}`)
             .then((response) => {
                 return response.json();
@@ -17,9 +19,30 @@ function Home() {
             })
     }, []);
 
+    // Lösung 1: if-statement
+    // if (articles !== undefined) {
+    //     return (
+    //         <section>
+    //             {articles.map((article, index) => <NewsArticle key={index} imgUrl={article.urlToImage} title={article.title} description={article.description} publishedAt={article.publishedAt} linkToArticle={article.url} />)}
+    //         </section>
+    //     );
+    // } else {
+    //     return <section><p>Loading...</p></section>
+    // }
+
+    // Lösung 2: Ternary Operator
+
+    // return (
+    //     <section>
+    //         {articles !== undefined ? articles.map((article, index) => <NewsArticle key={index} imgUrl={article.urlToImage} title={article.title} description={article.description} publishedAt={article.publishedAt} linkToArticle={article.url} />) : <p>Loading...</p> }
+    //     </section>
+    // );
+    
+    // Lösung 3: Optional chaining
+
     return (
         <section>
-            {articles.map((article, index) => <NewsArticle key={index} imgUrl={article.urlToImage} title={article.title} description={article.description} publishedAt={article.publishedAt} linkToArticle={article.url} />)}
+            {articles?.map((article, index) => <NewsArticle key={index} imgUrl={article.urlToImage} title={article.title} description={article.description} publishedAt={article.publishedAt} linkToArticle={article.url} />)}
         </section>
     );
 }
