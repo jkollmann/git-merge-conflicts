522843c0033c2042c13508e4b3b79242c9f976ff
SuperCoder <supercoder@SuperCoders-MacBook-Air.local>
2022-12-07 09:38:49 +0100

    Add filter by country

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,21 +4,26 @@ import "./Home.css";
 
 function Home() {
     const [articles, setArticles] = useState([]);
+    const [country, setCountry] = useState("de");
 
     useEffect(() => {
         console.log("Getting new articles from the server");
 
-        fetch(`https://newsapi.org/v2/top-headlines?country=de&apiKey=${process.env.REACT_APP_API_KEY}`)
+        fetch(`https://newsapi.org/v2/top-headlines?country=${country ? country : "de"}&apiKey=${process.env.REACT_APP_API_KEY}`)
             .then((response) => {
                 return response.json();
             })
             .then((articlesJson) => {
                 setArticles(articlesJson.articles);
             })
-    }, []);
+    }, [country]);
 
     return (
         <section>
+            <input
+                type="text"
+                onChange={(e) => setCountry(e.target.value)}
+            />
             {articles.map((article, index) => <NewsArticle key={index} imgUrl={article.urlToImage} title={article.title} description={article.description} publishedAt={article.publishedAt} linkToArticle={article.url} />)}
         </section>
     );
